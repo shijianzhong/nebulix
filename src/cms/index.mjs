a39a907c754315ec0c6477c69fe738d46e1b92ce
shijianzhong <994129509@qq.com>
2024-01-05 09:21:23 +0800

    add 美图频道

--- src/cms/index.mjs
+++ src/cms/index.mjs
@@ -4,5 +4,6 @@ import { post } from "./post.mjs";
 import { menu } from "./menu.mjs";
 import { project } from "./project.mjs";
 import { product } from "./product.mjs";
+import { gallery } from "./gallery.mjs";
 
-export { settings, menu, page, post, project, product };
+export { settings, menu, page, post, project, product,gallery };
