105e027e28c084771342c73fbb3973272e376128
shijianzhong <994129509@qq.com>
2024-01-05 16:53:57 +0800

    add pdf preview

---
+++ src/components/common/PdfView.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="pdf_container">
+      <iframe
+        :src="`pdf/web/viewer.html?file=${pdfUrl}`"
+        class="pdf-window"
+        width="100%"
+        height="100%"
+        frameborder="0"
+      ></iframe>
+    </div>
+</template>
+  
+<script setup>
+import {ref} from "vue"
+const pdfUrl = ref("../jianying.pdf")
+
+
+</script>
+<style scoped>
+.pdf_container{
+  width: 100%;
+  height: 1000px;
+}
+iframe{
+    width: 100%;
+    height: 100%;
+}
+</style>
